6ca4bc6118e1074052d465ef445704108b69b128
IGOR <sahmen@mail.ru>
2022-12-12 23:33:20 +0300

    DZ 1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите количество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] number = new int[M];
+
+for (int i = 0; i < M; i++)
+{
+Console.Write($"Введи число {i+1}: ");
+number[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+for (int i = 0; i < number.Length; i++)
+{
+if (number[i] > 0)
+count++;
+}
+Console.WriteLine($"Введено чисел больше 0: {count}");
