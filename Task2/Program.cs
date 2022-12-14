bdb937d33290b3e9fd654a514958703effc0cffe
IGOR <sahmen@mail.ru>
2022-12-14 18:47:12 +0300

    DZ 2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double n1 = -(b1 - b2);
+double n2 = k1 - k2;
+double x = n1 / n2;
+double y = k1 * x + b1; 
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+
+
+
+
+
+
+
